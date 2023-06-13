1d440ef511fe8b1f13cf32b84f396473fcf98c9a
Dimitar Yanchev <dimitar.yanchev@ampeco.com>
2023-06-13 09:26:14 +0300

    feat:Change Card model [sc-70665]

--- src/Models/Card.php
+++ src/Models/Card.php
@@ -8,12 +8,18 @@ class Card
     public string $token;
     public string $number;
     public string $lastFour;
+    public ?string $month;
+    public ?string $year;
+    public ?bool $isExpired;
 
     public function __construct(array $data)
     {
         $this->cardType = $data['cardType'];
         $this->token = $data['cardToken'];
         $this->number = $data['maskedCardNumber'];
+        $this->month = $data['lastMonth'] ?? null;
+        $this->year = $data['lastYear'] ?? null;
+        $this->isExpired = $data['isExpired'] ?? null;
 
         $this->lastFour = substr($this->number, -4);
     }
